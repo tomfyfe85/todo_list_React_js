39cfd109f6dfb260ed3efa0f34010087210dc9c6
Tom Fyfe <tomfyfe85@gmail.com>
2023-12-14 18:12:33 +0000

    added useState - when input is added in the field it is saved as state on change

--- src/components/PracticeTodo.jsx
+++ src/components/PracticeTodo.jsx
@@ -1,9 +1,32 @@
+import React, { useState } from 'react'
+
 const PracticeTodo = () => {
 const todoArray = [{id:1, todo:"todo 1"}, {id:2, todo:"todo 2"}]
 
+const [todos, setTodos] = useState(todoArray)
+const [inputValue, setInputValue] = useState("")
+  
+  
+  const handleChange = (event) => {
+    setInputValue(event.target.value)
+  };
+  
+  console.log(inputValue)
+
   return (<>
     <h1>Practice Todo</h1>
-    {todoArray.map((todo) => (
+    <form>
+      <label htmlFor="addTodo">
+        Add a TodoList
+        <input type='todo' name='submit' value={inputValue} onChange={handleChange}/>
+      </label>
+      <label>
+        <button type = "submit" name ="submit">
+          Submit
+        </button>
+      </label>
+    </form>
+    {todos.map((todo) => (
       <div key={todo.id}>
         {todo.todo}
       </div>
