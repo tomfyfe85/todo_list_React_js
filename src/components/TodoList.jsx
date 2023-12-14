0866198601c306898ff461ac01d6dec7b405db73
Tom Fyfe <tomfyfe85@gmail.com>
2023-12-14 01:00:21 +0000

    imput field clears on submit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,30 +7,39 @@ const TodoList = () => {
   ];
   const [todos, setTodos] = useState(todosArray);
   const [newTodo, setNewTodo] = useState("");
-  const [idCounter, setIdCounter] = useState(3)
+  const [idCounter, setIdCounter] = useState(3);
   // const addTodo = (newTodo, setTodos, todos) => {
   //   setTodos([...todos, { id: 3, todo: newTodo }]);
   // ()=>addTodo(newTodo, setTodos, todos)
   // };
 
-  
-  const addTodo = (event, newTodo, todos, setTodos, idCounter, setIdCounter) => {
-    event.preventDefault()
+  const addTodo = (
+    event,
+    newTodo,
+    todos,
+    setTodos,
+    idCounter,
+    setIdCounter
+  ) => {
+    event.preventDefault();
     setTodos([...todos, { id: idCounter, todo: newTodo }]);
-    setIdCounter(idCounter + 1)
-    
-    };
-    
-  
+    setIdCounter(idCounter + 1);
+    setNewTodo("");
+  };
+
   const handleChange = (event) => {
     setNewTodo(event.target.value);
   };
-  console.log(todos)
+  console.log(todos);
 
   return (
     <div>
       <h1>Todo List</h1>
-      <form onSubmit={()=>addTodo(event, newTodo, todos, setTodos, idCounter, setIdCounter)}>
+      <form
+        onSubmit={() =>
+          addTodo(event, newTodo, todos, setTodos, idCounter, setIdCounter)
+        }
+      >
         <label htmlFor="addTodo">
           Add a todo:
           <input
