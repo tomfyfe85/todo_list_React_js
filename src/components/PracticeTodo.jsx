74b17e2afa129fd968af841265e135d8ad200fa7
Tom Fyfe <tomfyfe85@gmail.com>
2023-12-18 23:58:58 +0000

    add() function needs comleteing

--- src/components/PracticeTodo.jsx
+++ src/components/PracticeTodo.jsx
@@ -6,16 +6,24 @@ const todoArray = [{id:1, todo:"todo 1"}, {id:2, todo:"todo 2"}]
 const [todos, setTodos] = useState(todoArray)
 const [inputValue, setInputValue] = useState("")
   
-  
+  const add = (event, setTodos) => {
+    event.preventDefault()
+
+    setTodos([...todos, {
+      id: 3, todo: inputValue
+    }])
+    // console.log(todos)
+
+  }
+  console.log(todos)
+
   const handleChange = (event) => {
     setInputValue(event.target.value)
   };
   
-  console.log(inputValue)
-
   return (<>
     <h1>Practice Todo</h1>
-    <form>
+    <form onSubmit={() => add(setTodos)}>
       <label htmlFor="addTodo">
         Add a TodoList
         <input type='todo' name='submit' value={inputValue} onChange={handleChange}/>
