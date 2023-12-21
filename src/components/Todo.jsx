e5f52018ff1edabdf69dd9af21c1be37251584a5
Tom Fyfe <tomfyfe85@gmail.com>
2023-12-21 10:10:07 +0000

    added delete button functionality in Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,23 @@
+const Todo = ({ id, todo, todos, setTodos }) => {
+  const deleteTodo = (id, todos, setTodos) => {
+    // console.log('Delete todo')
+    let deleted = [];
+    todos.forEach((todo) => {
+      if (todo.id != id) {
+        deleted.push(todo);
+      }
+      setTodos(deleted);
 
-const Todo = ({ id, todo }) => {
+    });
+  };
+
+
   return (
     <>
       <div>{todo}</div>
-      <button>Delete</button>
-  </>);
-}
- 
+      <button onClick={() => deleteTodo(id, todos, setTodos)}>Delete</button>
+    </>
+  );
+};
+
 export default Todo;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,35 +1,30 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import Todo from "./Todo";
 const TodoList = () => {
-
-  const [todos, setTodos] = useState([{id:1 , todo: 'todo'}]);
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
-  const [idCounter, setIdCounter] = useState(0);
-
+  const [idCounter, setIdCounter] = useState(1);
 
   const addTodo = (event) => {
     event.preventDefault();
     setIdCounter(idCounter + 1);
     setTodos([...todos, { id: idCounter, todo: newTodo }]);
     setNewTodo("");
-  };
-
+    console.log(todos)
+};
   const deleteAll = () => {
-    setTodos([])
-  }
-    console.log(newTodo)
+    setTodos([]);
+  };
+  console.log(todos)
 
   const handleChange = (event) => {
     setNewTodo(event.target.value);
   };
-  console.log(todos);
 
   return (
     <div>
       <h1>Todo List</h1>
-      <form
-        onSubmit={addTodo}
-      >
+      <form onSubmit={addTodo}>
         <label htmlFor="addTodo">
           Add a todo:
           <input
@@ -44,12 +39,12 @@ const TodoList = () => {
           SUBMIT
         </button>
       </form>
-      <button onClick={deleteAll} id='delete'>
-          DELETE ALL
-        </button>
+      <button onClick={deleteAll} id="delete">
+        DELETE ALL
+      </button>
       {todos.map((todo) => (
         <div key={todo.id}>
-          <Todo id={todo.id} todo={todo.todo}/>
+          <Todo id={todo.id} todo={todo.todo} todos={todos} setTodos={setTodos} />
         </div>
       ))}
     </div>
