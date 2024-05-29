19770fa9c87b4eb549918c8247ddcbe728695f99
Tom Fyfe <tomfyfe85@gmail.com>
2024-05-29 22:53:56 +0100

    updates

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,5 @@
 import MotivationQuote from "./MotivationQuote";
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
 const Todo = ({
   id,
